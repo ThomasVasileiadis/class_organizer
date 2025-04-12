104313541b960a3c4ff9c7056fe7cfac0bafe2fe
winrthelegend23 <winrthelegend23@gmail.com>
2025-04-12 08:01:25 +0000

    Added

--- Data/ScheduleModel.cs
+++ Data/ScheduleModel.cs
@@ -1,14 +1,1 @@
 csharp
-using System;
-
-namespace class_organizer.Data
-{
-    public class ScheduleModel
-    {
-        public ClassModel Class { get; set; }
-        public TeacherModel Teacher { get; set; }
-        public StudentModel Student { get; set; }
-        public DateTime StartTime { get; set; }
-        public DateTime EndTime { get; set; }
-    }
-}
--- Data/ScheduleService.cs
+++ Data/ScheduleService.cs
@@ -4,111 +4,120 @@ using System.Collections.Generic;
 using System.Linq;
 using class_organizer.Data;
 
-public class ScheduleService
+namespace class_organizer.Data
 {
-    private readonly StudentService _studentService;
-    private readonly TeacherService _teacherService;
-    private readonly ClassService _classService;
-
-    public ScheduleService(StudentService studentService, TeacherService teacherService, ClassService classService)
+    public class ScheduleService
     {
-        _studentService = studentService;
-        _teacherService = teacherService;
-        _classService = classService;
-    }
+        private readonly StudentService _studentService;
+        private readonly TeacherService _teacherService;
+        private readonly ClassService _classService;
 
-    public List<ScheduleModel> GenerateWeeklySchedule()
-    {
-        var schedule = new List<ScheduleModel>();
-        var timeSlots = GetTimeSlots();
-        var daysOfWeek = Enum.GetValues(typeof(DayOfWeek)).Cast<DayOfWeek>().Where(d => d != DayOfWeek.Saturday && d != DayOfWeek.Sunday).ToList();
-
-        var classes = _classService.GetClasses();
-        var teachers = _teacherService.GetTeachers();
-        var students = _studentService.GetStudents();
-
-        var classIndex = 0;
-        foreach (var day in daysOfWeek)
+        public ScheduleService(StudentService studentService, TeacherService teacherService, ClassService classService)
         {
-            foreach (var timeSlot in timeSlots)
-            {
-                if (classIndex >= classes.Count)
-                {
-                    break; // No more classes to schedule
-                }
-
-                var currentClass = classes[classIndex];
-                var availableTeachers = teachers.Where(t => t.CanTeachClass(currentClass.Name) && !IsTeacherBooked(schedule, day, timeSlot, t.Id)).ToList();
-
-                if (availableTeachers.Any())
-                {
-                    var teacher = availableTeachers.First(); // Simple strategy: pick the first available teacher
-                    var enrolledStudents = students.Where(s => s.ClassId == currentClass.Id && !IsStudentBooked(schedule, day, timeSlot, s.Id)).ToList();
-
-                    if (enrolledStudents.Any())
-                    {
-                        schedule.Add(new ScheduleModel
-                        {
-                            Class = currentClass,
-                            Teacher = teacher,
-                            StartTime = timeSlot.startTime.ToDateTime(DateTime.Today.AddDays(((int)day - (int)DateTime.Today.DayOfWeek + 7) % 7)),
-                            EndTime = timeSlot.endTime.ToDateTime(DateTime.Today.AddDays(((int)day - (int)DateTime.Today.DayOfWeek + 7) % 7)),
-                            Students = enrolledStudents
-                        });
-                    }
-                }
-
-                classIndex++;
-            }
+            _studentService = studentService;
+            _teacherService = teacherService;
+            _classService = classService;
         }
 
-        return schedule;
-    }
-
-    private bool IsTeacherBooked(List<ScheduleModel> schedule, DayOfWeek day, (TimeOnly startTime, TimeOnly endTime) timeSlot, int teacherId)
-    {
-        return schedule.Any(s =>
-            s.Teacher.Id == teacherId &&
-            s.StartTime.DayOfWeek == day &&
-            ((timeSlot.startTime >= TimeOnly.FromDateTime(s.StartTime) && timeSlot.startTime < TimeOnly.FromDateTime(s.EndTime)) ||
-             (timeSlot.endTime > TimeOnly.FromDateTime(s.StartTime) && timeSlot.endTime <= TimeOnly.FromDateTime(s.EndTime)) ||
-             (TimeOnly.FromDateTime(s.StartTime) >= timeSlot.startTime && TimeOnly.FromDateTime(s.StartTime) < timeSlot.endTime) ||
-             (TimeOnly.FromDateTime(s.EndTime) > timeSlot.startTime && TimeOnly.FromDateTime(s.EndTime) <= timeSlot.endTime)));
-    }
-
-    private bool IsStudentBooked(List<ScheduleModel> schedule, DayOfWeek day, (TimeOnly startTime, TimeOnly endTime) timeSlot, int studentId)
-    {
-        return schedule.Any(s =>
-            s.Students.Any(student => student.Id == studentId) &&
-            s.StartTime.DayOfWeek == day &&
-            ((timeSlot.startTime >= TimeOnly.FromDateTime(s.StartTime) && timeSlot.startTime < TimeOnly.FromDateTime(s.EndTime)) ||
-             (timeSlot.endTime > TimeOnly.FromDateTime(s.StartTime) && timeSlot.endTime <= TimeOnly.FromDateTime(s.EndTime)) ||
-             (TimeOnly.FromDateTime(s.StartTime) >= timeSlot.startTime && TimeOnly.FromDateTime(s.StartTime) < timeSlot.endTime) ||
-             (TimeOnly.FromDateTime(s.EndTime) > timeSlot.startTime && TimeOnly.FromDateTime(s.EndTime) <= timeSlot.endTime)));
-    }
-
-    private List<(TimeOnly startTime, TimeOnly endTime)> GetTimeSlots()
-    {
-        return new List<(TimeOnly, TimeOnly)>
+        public List<ScheduleModel> GenerateWeeklySchedule()
         {
-            (new TimeOnly(9, 0), new TimeOnly(10, 0)),
-            (new TimeOnly(10, 0), new TimeOnly(11, 0)),
-            (new TimeOnly(11, 0), new TimeOnly(12, 0)),
-            (new TimeOnly(13, 0), new TimeOnly(14, 0)),
-            (new TimeOnly(14, 0), new TimeOnly(15, 0)),
-            (new TimeOnly(15, 0), new TimeOnly(16, 0)),
-            (new TimeOnly(16, 0), new TimeOnly(17, 0))
-        };
+            var schedule = new List<ScheduleModel>();
+            var timeSlots = GetTimeSlots();
+            var daysOfWeek = Enum.GetValues(typeof(DayOfWeek)).Cast<DayOfWeek>().Where(d => d != DayOfWeek.Saturday && d != DayOfWeek.Sunday).ToList();
+
+            var classes = _classService.GetClasses();
+            var teachers = _teacherService.GetTeachers();
+            var students = _studentService.GetStudents();
+
+            var classIndex = 0;
+            foreach (var day in daysOfWeek)
+            {
+                foreach (var timeSlot in timeSlots)
+                {
+                    if (classIndex >= classes.Count)
+                    {
+                        break; // No more classes to schedule
+                    }
+
+                    var currentClass = classes[classIndex];
+                    var availableTeachers = teachers.Where(t => t.CanTeachClass(currentClass.Name) && !IsTeacherBooked(schedule, day, timeSlot, t.Id)).ToList();
+
+                    if (availableTeachers.Any())
+                    {
+                        var teacher = availableTeachers.First(); // Simple strategy: pick the first available teacher
+                        var enrolledStudents = students.Where(s => s.ClassId == currentClass.Id && !IsStudentBooked(schedule, day, timeSlot, s.Id)).ToList();
+
+                        if (enrolledStudents.Any())
+                        {
+                            schedule.Add(new ScheduleModel
+                            {
+                                Class = currentClass,
+                                Teacher = teacher,
+                                StartTime = timeSlot.startTime.ToDateTime(DateTime.Today.AddDays(((int)day - (int)DateTime.Today.DayOfWeek + 7) % 7)),
+                                EndTime = timeSlot.endTime.ToDateTime(DateTime.Today.AddDays(((int)day - (int)DateTime.Today.DayOfWeek + 7) % 7)),
+                                Students = enrolledStudents
+                            });
+                        }
+                    }
+
+                    classIndex++;
+                }
+            }
+
+            return schedule;
+        }
+
+        private bool IsTeacherBooked(List<ScheduleModel> schedule, DayOfWeek day, (TimeOnly startTime, TimeOnly endTime) timeSlot, int teacherId)
+        {
+            return schedule.Any(s =>
+                s.Teacher.Id == teacherId &&
+                s.StartTime.DayOfWeek == day &&
+                ((timeSlot.startTime >= TimeOnly.FromDateTime(s.StartTime) && timeSlot.startTime < TimeOnly.FromDateTime(s.EndTime)) ||
+                 (timeSlot.endTime > TimeOnly.FromDateTime(s.StartTime) && timeSlot.endTime <= TimeOnly.FromDateTime(s.EndTime)) ||
+                 (TimeOnly.FromDateTime(s.StartTime) >= timeSlot.startTime && TimeOnly.FromDateTime(s.StartTime) < timeSlot.endTime) ||
+                 (TimeOnly.FromDateTime(s.EndTime) > timeSlot.startTime && TimeOnly.FromDateTime(s.EndTime) <= timeSlot.endTime)));
+        }
+
+        private bool IsStudentBooked(List<ScheduleModel> schedule, DayOfWeek day, (TimeOnly startTime, TimeOnly endTime) timeSlot, int studentId)
+        {
+            return schedule.Any(s =>
+                s.Students.Any(student => student.Id == studentId) &&
+                s.StartTime.DayOfWeek == day &&
+                ((timeSlot.startTime >= TimeOnly.FromDateTime(s.StartTime) && timeSlot.startTime < TimeOnly.FromDateTime(s.EndTime)) ||
+                 (timeSlot.endTime > TimeOnly.FromDateTime(s.StartTime) && timeSlot.endTime <= TimeOnly.FromDateTime(s.EndTime)) ||
+                 (TimeOnly.FromDateTime(s.StartTime) >= timeSlot.startTime && TimeOnly.FromDateTime(s.StartTime) < timeSlot.endTime) ||
+                 (TimeOnly.FromDateTime(s.EndTime) > timeSlot.startTime && TimeOnly.FromDateTime(s.EndTime) <= timeSlot.endTime)));
+        }
+
+        private List<(TimeOnly startTime, TimeOnly endTime)> GetTimeSlots()
+        {
+            return new List<(TimeOnly, TimeOnly)>
+            {
+                (new TimeOnly(9, 0), new TimeOnly(10, 0)),
+                (new TimeOnly(10, 0), new TimeOnly(11, 0)),
+                (new TimeOnly(11, 0), new TimeOnly(12, 0)),
+                (new TimeOnly(13, 0), new TimeOnly(14, 0)),
+                (new TimeOnly(14, 0), new TimeOnly(15, 0)),
+                (new TimeOnly(15, 0), new TimeOnly(16, 0)),
+                (new TimeOnly(16, 0), new TimeOnly(17, 0))
+            };
+        }
+
+        public class Schedule
+        {
+            public List<ScheduleModel> ScheduledEvents { get; set; }
+        }
+    }
+
+    public static class TimeOnlyExtensions
+    {
+        public static DateTime ToDateTime(this TimeOnly time, DateTime date)
+        {
+            return date.Date + time.ToTimeSpan();
+        }
     }
 }
 
-public static class TimeOnlyExtensions
-{
-    public static DateTime ToDateTime(this TimeOnly time, DateTime date)
-    {
-        return date.Date + time.ToTimeSpan();
-    }
-}
 
 
 
